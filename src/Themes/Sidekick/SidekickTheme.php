748f0ef831ffdee5c7b38d3803a53c4a3dc450ec
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-05-29 09:57:20 +0100

    Sidekick Theme initial commit

---
+++ src/Themes/Sidekick/SidekickTheme.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author oke.ugwu
+ */
+
+namespace Themes\Sidekick;
+
+use Cubex\Theme\BaseTheme;
+
+class SidekickTheme extends BaseTheme
+{
+  protected function _initiate()
+  {
+    $this->requireCssLibrary("bootstrap", "2");
+    $this->requireCss("/bs-reset");
+    $this->requireCss("/base");
+
+    $this->requireJsLibrary("jquery");
+    $this->requireJsLibrary("bootstrap", "2");
+    $this->requireJsPackage("bootstrap");
+    $this->requireJs("/base");
+  }
+}
---
+++ src/Themes/Sidekick/res/js/base.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: oke.ugwu
+ * Date: 28/05/13
+ * Time: 08:38
+ * To change this template use File | Settings | File Templates.
+ */
+$('#header a').tooltip();
